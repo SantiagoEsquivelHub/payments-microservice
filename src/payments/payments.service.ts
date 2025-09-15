b6499dd0fe56f9a13fb92fc327ff0966014cf0cb
Santiago Sánchez Esquivel <ssancheze@siesa.com>
2025-09-15 17:36:44 -0500

    SS - Payments Microservice

---
+++ src/payments/payments.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@nestjs/common';
+import { envs } from 'src/config';
+import Stripe from 'stripe';
+import { PaymentSessionDto } from './dto/payment-session.dto';
+import { Request, Response } from 'express';
+
+@Injectable()
+export class PaymentsService {
+
+    private readonly stripe = new Stripe(envs.stripeSecret);
+
+    async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
+
+        const { currency, items } = paymentSessionDto;
+
+        const lineItems = items.map(item => {
+            return {
+                price_data: {
+                    currency: currency,
+                    product_data: {
+                        name: item.name
+                    },
+                    unit_amount: Math.round(item.price * 100)
+                },
+                quantity: item.quantity
+            }
+        });
+
+        const session = await this.stripe.checkout.sessions.create({
+            payment_intent_data: {
+                metadata: {}
+            },
+            line_items: lineItems,
+            mode: 'payment',
+            success_url: envs.stripeSuccessUrl,
+            cancel_url: envs.stripeCancelUrl
+        });
+
+        return session;
+    }
+
+    async stripeWebhook(req: Request, res: Response) {
+        const sig = req.headers['stripe-signature']!;
+        let event: Stripe.Event;
+
+        const endpointSecret = envs.stripeEndpointSecret;
+
+        try {
+            event = this.stripe.webhooks.constructEvent(
+                req['rawBody'],
+                sig,
+                endpointSecret,
+            );
+        } catch (err) {
+            res.status(400).send(`Webhook Error: ${err.message}`);
+            return;
+        }
+
+        return res.status(200).json({ sig });
+    }
+}
